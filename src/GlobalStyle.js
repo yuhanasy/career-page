cc2d6e5194bc771aba426f504571fb1bb0c8cbd9
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2019-03-20 21:47:19 +0700

    Add Global Style

---
+++ src/GlobalStyle.js
@@ -0,0 +1,19 @@
+/*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
+
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    line-height: 1.15;
+  }
+
+  html, body {
+    margin: 0;
+    padding: 0;
+    line-height: 1.15;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+`;
