fa71dc46e4fbf610d59c7c54dc0d54f6e4933a26
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2019-03-25 06:36:29 +0700

    add interaction for testimonial section

--- src/components/Testimoni/index.js
+++ src/components/Testimoni/index.js
@@ -1,20 +1,55 @@
 import React from "react";
 
 import { Section } from "../../shared/layout/styles";
-import { CarouselWrapper, PeoplePict } from "./styles";
-import Carousel from "../Carousel";
+import {
+  CarouselWrapper,
+  CarouselTitle,
+  CarouselContent,
+  CarouselPict,
+  Dot,
+  CarouselCredit
+} from "./styles";
 
-import people from "../../images/people-1.jpg";
+import data from "./constants.json";
+const pictures = require.context("../../images/", true);
 
-const Testimoni = () => {
-  return (
-    <Section>
-      <CarouselWrapper>
-        <Carousel />
-        <PeoplePict src={people} />
-      </CarouselWrapper>
-    </Section>
-  );
-};
+class Testimoni extends React.Component {
+  state = {
+    id: "1"
+  };
+
+  handleClick = e => {
+    const id = e.target.id;
+    this.setState({ id });
+  };
+
+  render() {
+    const { testimoni } = data;
+    const { id } = this.state;
+    return (
+      <Section>
+        <CarouselTitle>Hear from nodefux</CarouselTitle>
+        {testimoni
+          .filter(d => d.id === id)
+          .map(d => (
+            <CarouselWrapper key={d.id}>
+              <div>
+                <CarouselContent>{`"${d.content}"`}</CarouselContent>
+                <CarouselCredit>
+                  <span>{d.name}</span>
+                  {`, `}
+                  {d.position}
+                </CarouselCredit>
+              </div>
+              <CarouselPict src={pictures(`./people-${d.id}.jpg`)} />
+            </CarouselWrapper>
+          ))}
+        {testimoni.map(d => (
+          <Dot id={d.id} onClick={this.handleClick} key={d.id} />
+        ))}
+      </Section>
+    );
+  }
+}
 
 export default Testimoni;
--- src/components/Testimoni/styles.js
+++ src/components/Testimoni/styles.js
@@ -1,13 +1,52 @@
 import styled from "styled-components";
 
+import { InfoTitle, Paragraph } from "../../shared/typography/styles";
+
 export const CarouselWrapper = styled.div`
   width: 100%;
   display: grid;
   grid-template-columns: 1fr 320px;
 `;
 
-export const PeoplePict = styled.img`
+export const CarouselTitle = styled(InfoTitle)`
+  font-size: 24px;
+  color: #3e396b;
+`;
+
+export const CarouselContent = styled(Paragraph)`
+  font-size: 32px;
+  line-height: 1.5;
+  color: #ae90cb;
+  padding-right: 20%;
+`;
+
+export const CarouselPict = styled.img`
   width: 100%;
   max-height: 400px;
   border-radius: 4px;
+`;
+
+export const CarouselCredit = styled(Paragraph)`
+  color: #ae90cb;
+  font-weight: 400;
+
+  span {
+    color: #3e396b;
+    font-weight: 500;
+  }
+`;
+
+export const Dot = styled.span`
+  cursor: pointer;
+  height: 8px;
+  width: 8px;
+  margin: 0 8px;
+  background-color: #ae90cb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.2s ease;
+
+  :hover {
+    background-color: #3e396b;
+  }
 `;
